cf4c784fb451b69b35b7a5306c74f75313d1b239
Brendan-Warner <80108372+Brendan-Warner@users.noreply.github.com>
2022-12-30 19:50:07 -0600

    Update merge_sort.h

--- merge_sort.h
+++ merge_sort.h
@@ -1,7 +1,7 @@
 #pragma once
 
-vector<arr_int*> merge(vector<int_arr*> to_sort_left, vector<int_arr*> to_sort_right);
+vector<arr_int*> merge(vector<int> to_sort_left, vector<int> to_sort_right);
 
-vector<arr_int*> merg_sort(vector<int_arr*> to_sort);
+vector<arr_int*> merg_sort(vector<int> to_sort);
 
 void selection_sort(int size, int output, bool file);
