b40e33f189f85d062c11cdbc32fe40d11fcb17e0
Brendan-Warner <80108372+Brendan-Warner@users.noreply.github.com>
2023-01-01 15:31:50 -0600

    Update insertion_sort.cpp

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -12,50 +12,57 @@ using namespace std;
 //larger then the current, then we know all values before that previous value are also smaller then our current, so we have no need to check them. the time complexity is O(n).
 void insertion_sort(int size, int output, bool file)
 {
-	
-	int user_choice();
+
+	bool user = user_choice();
 	int max_num = 0;
 	int screen_output = 0;
 	vector<int> to_sort;
-	if(user_choice)
+	if (user)
 	{
-		create_array_user(to_sort, max_num);
+		create_array_user(to_sort, max_num, size);
 	}
-	
+
 	else
 	{
 		cout << "please enter a max number" << endl;
 		cin >> max_num;
-		create_array(to_sort, max_num)
+		create_array(to_sort, max_num, size);
 	}
-	int i = 0; j = 0;
-	for(i = 0; i < to_sort.size(); i ++)
+
+	print_array(to_sort, max_num);
+
+	int i = 0, j = 0;
+	for (i = 0; i < to_sort.size(); i++)
 	{
-		for(j = i-1; j >=0; j--)
+		int t = i;
+		for (j = i - 1; j >= 0; j--)
 		{
-			if(to_sort[i] < to_sort[j])
+			
+			if (to_sort[t] < to_sort[j])
 			{
-				int temp = to_sort[i];
-				to_sort[i] = to_sort[j];
+				int temp = to_sort[t];
+				to_sort[t] = to_sort[j];
 				to_sort[j] = temp;
-				
+				t--;
+
 				screen_output++;
-				if(output == screen_output)
+				if (output == screen_output)
 				{
 					print_array(to_sort, max_num);
 					screen_output = 0;
 				}
-				
+
 			}
-			
+
 			else
 			{
 				break;
 			}
 		}
 	}
-	
+
 }
+
 
 
 
