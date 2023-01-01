5a8888863f5650660b92faeaa1c4e492c44b0435
Brendan-Warner <80108372+Brendan-Warner@users.noreply.github.com>
2023-01-01 15:31:27 -0600

    Update bubble_sort.cpp

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -10,49 +10,52 @@ using namespace std;
 
 void bubble_sort(int size, int output, bool file)
 {
-	
-	int user_choice();
+
+	bool user =  user_choice();
 	int max_num = 0;
 	int screen_output = 0;
-	int to_sort;
-	if(user_choice)
+	vector<int> to_sort;
+	if (user)
 	{
 		create_array_user(to_sort, max_num, size);
 	}
-	
+
 	else
 	{
 		cout << "please enter a max number" << endl;
 		cin >> max_num;
-		create_array(to_sort, max_num, size)
+		create_array(to_sort, max_num, size);
 	}
-	
+
+	print_array(to_sort, max_num);
+
 	//this is the main for loop, with bubble sort, the idea is that larger numbers in the array "bubble" to the top of the array. the complexity of bubble sort is On^2
-	int i = 0; j = 0;
-	for(i = 0; i < to_sort.size(); i ++)
+	int i = 0, j = 0;
+	for (i = 0; i < to_sort.size(); i++)
 	{
-		for(j = 0; j < to_sort.size() - 1; j ++)
+		for (j = 0; j < to_sort.size() - 1; j++)
 		{
-			if(to_sort[j] > to_sort[j+1])
+			if (to_sort[j] > to_sort[j + 1])
 			{
 				int temp = to_sort[j];
-				to_sort[j] = to_sort[j+1];
-				to_sort[j+1] = temp;
+				to_sort[j] = to_sort[j + 1];
+				to_sort[j + 1] = temp;
 				screen_output++;
-				
-				
-				if(screen_output == output)
+
+
+				if (screen_output == output)
 				{
 					screen_output = 0;
-					print_array(to_sort, max_num, file);
+					print_array(to_sort, max_num);
 				}
-				
+
 			}
-			
+
 		}
 	}
-	
+
 }
+
 
 
 
