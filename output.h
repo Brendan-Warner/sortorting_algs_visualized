0ace928727c2f8eca56803be52109ceea5b68c0b
Brendan-Warner <80108372+Brendan-Warner@users.noreply.github.com>
2023-01-04 14:23:15 -0600

    Update output.h

--- output.h
+++ output.h
@@ -1,8 +1,14 @@
 #pragma once
-int user_choice();
+#include <vector>
 
-create_array_user(vector<int_arr>& to_sort, int max, int size);
+bool user_choice();
 
-create_array(vector<int_arr>& to_sort, int max, int size);
+void create_array_user(std::vector<int>& to_sort, int& max, int size);
 
-void print_array(vector<int_arr> to_sort, int max);
+void create_array(std::vector<int>& to_sort, int max, int size);
+
+void print_array(std::vector<int> to_sort, int max);
+
+void print_array(std::vector<int> to_sort, int max, int to_swap_1, int to_swap_2);
+
+void print_array_merge(std::vector<int> combined_vec, std::vector<int> working_vec_left, std::vector<int> working_vec_right, int max);
