096b6aefd42c877097a12ca348187ea43e044e15
Brendan-Warner <80108372+Brendan-Warner@users.noreply.github.com>
2023-01-10 20:41:08 -0600

    Update merge_sort.cpp
    updated for file output.

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 
 //this is where mergsort combines to partionted parts of the orginal vector.
-vector<int> merge(vector<int> to_sort_left, vector<int> to_sort_right, int max)
+vector<int> merge(vector<int> to_sort_left, vector<int> to_sort_right, int max, bool output)
 {
 	vector<int> combined_vects;
 	while (!to_sort_left.empty() && !to_sort_right.empty())//checks if one of the partions is empty.
@@ -18,13 +18,13 @@ vector<int> merge(vector<int> to_sort_left, vector<int> to_sort_right, int max)
 		if (to_sort_left[0] < to_sort_right[0])//if the left partion number is smaller, then add it to the vector, else we add the right number to the vector.
 		{
 			combined_vects.push_back(to_sort_left[0]);
-			print_array_merge(combined_vects,to_sort_left, to_sort_right, max);
+			print_array_merge(combined_vects,to_sort_left, to_sort_right, max, output);
 			to_sort_left.erase(to_sort_left.begin());
 		}
 
 		else {
 			combined_vects.push_back(to_sort_right[0]);
-			print_array_merge(combined_vects, to_sort_left, to_sort_right, max);
+			print_array_merge(combined_vects, to_sort_left, to_sort_right, max, output);
 			to_sort_right.erase(to_sort_right.begin());
 		}
 
@@ -33,14 +33,14 @@ vector<int> merge(vector<int> to_sort_left, vector<int> to_sort_right, int max)
 	while (!to_sort_left.empty())//if the left array is not empty but the right is, then we add the remaning left numbers to the array. These values should already be sorted by a previous merge.
 	{
 		combined_vects.push_back(to_sort_left[0]);
-		print_array_merge(combined_vects, to_sort_left, to_sort_right, max);
+		print_array_merge(combined_vects, to_sort_left, to_sort_right, max, output);
 		to_sort_left.erase(to_sort_left.begin());
 	}
 
 	while (!to_sort_right.empty())//if the right array is not empty but the left is, then we add the remaning right numbers to the array. These values should already be sorted by a previous merge.
 	{
 		combined_vects.push_back(to_sort_right[0]);
-		print_array_merge(combined_vects, to_sort_left, to_sort_right, max);
+		print_array_merge(combined_vects, to_sort_left, to_sort_right, max, output);
 		to_sort_right.erase(to_sort_right.begin());
 	}
 
@@ -49,7 +49,7 @@ vector<int> merge(vector<int> to_sort_left, vector<int> to_sort_right, int max)
 }
 
 //This is the other main portion of merge sort, it partions the vector recursivly until we are left with vectors of size one. after that, we use the merge function above to combine the vectors back into one large one. 
-vector<int> merg_sort(vector<int> to_sort, int max)
+vector<int> merg_sort(vector<int> to_sort, int max, bool output)
 {
 	if (to_sort.size() == 1)
 	{
@@ -69,10 +69,10 @@ vector<int> merg_sort(vector<int> to_sort, int max)
 		to_sort_right.push_back(to_sort[i]);
 	}
 	
-	to_sort_left = merg_sort(to_sort_left, max);
-	to_sort_right = merg_sort(to_sort_right, max);
+	to_sort_left = merg_sort(to_sort_left, max, output);
+	to_sort_right = merg_sort(to_sort_right, max, output);
 
-	return merge(to_sort_left, to_sort_right, max);
+	return merge(to_sort_left, to_sort_right, max, output);
 
 
 
@@ -102,11 +102,12 @@ void merge_sort_prep(int size, int output, bool file)
 	}
 	
 
-	print_array(to_sort, max_num);
+	print_array(to_sort, max_num, file);
 
-	to_sort = merg_sort(to_sort, max_num);
+	to_sort = merg_sort(to_sort, max_num, file);
 
 }
+
 
 
 
