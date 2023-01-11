e52a364f98fa0ca6034411d07a06a93f191422c9
Brendan-Warner <80108372+Brendan-Warner@users.noreply.github.com>
2023-01-10 20:43:57 -0600

    Update merge_sort.h

--- merge_sort.h
+++ merge_sort.h
@@ -1,8 +1,8 @@
 #pragma once
 #include <vector>
 
-std::vector<int> merge(std::vector<int> to_sort_left, std::vector<int> to_sort_right, int max);
+std::vector<int> merge(std::vector<int> to_sort_left, std::vector<int> to_sort_right, int max, bool output);
 
-std::vector<int> merg_sort(std::vector<int> to_sort, int max);
+std::vector<int> merg_sort(std::vector<int> to_sort, int max, bool output);
 
 void merge_sort_prep(int size, int output, bool file);
