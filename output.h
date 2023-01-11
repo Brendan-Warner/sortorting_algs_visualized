83472a7f6bceb75f5056fa67267d81961577a1ac
Brendan-Warner <80108372+Brendan-Warner@users.noreply.github.com>
2023-01-10 20:44:18 -0600

    Update output.h

--- output.h
+++ output.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+extern std::string file_output;
 
 bool user_choice();
 
@@ -7,8 +8,8 @@ void create_array_user(std::vector<int>& to_sort, int& max, int size);
 
 void create_array(std::vector<int>& to_sort, int max, int size);
 
-void print_array(std::vector<int> to_sort, int max);
+void print_array(std::vector<int> to_sort, int max, bool output);
 
-void print_array(std::vector<int> to_sort, int max, int to_swap_1, int to_swap_2);
+void print_array(std::vector<int> to_sort, int max, int to_swap_1, int to_swap_2, bool output);
 
-void print_array_merge(std::vector<int> combined_vec, std::vector<int> working_vec_left, std::vector<int> working_vec_right, int max);
+void print_array_merge(std::vector<int> combined_vec, std::vector<int> working_vec_left, std::vector<int> working_vec_right, int max, bool output);
